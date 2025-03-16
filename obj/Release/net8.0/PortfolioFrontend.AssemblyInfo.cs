d0a0a084c905237f6fafb4fb1ea91aca33bc0cf9
bhanusharma28 <bhanuhiet281100@gmail.com>
2025-03-16 14:27:14 +0530

    EmailChanges

--- obj/Release/net8.0/PortfolioFrontend.AssemblyInfo.cs
+++ obj/Release/net8.0/PortfolioFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8145a743c5ee759db01e9c1557dfc13eb6bf71f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b6c4aa649f3667dda3d57965596ad0ae98bdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
