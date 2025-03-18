c51de0caa852a4708173c788f478ac4ac95bd285
bhanusharma28 <bhanuhiet281100@gmail.com>
2025-03-19 01:28:55 +0530

    pageanimation

--- obj/Release/net8.0/PortfolioFrontend.AssemblyInfo.cs
+++ obj/Release/net8.0/PortfolioFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db2802691e13d981fdae184cc328981b9b20b267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe128fe3aadd4d70374ed7611cf2e8921e3b7a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
