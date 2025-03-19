875a25593da93b5bcc011debb666fe49329c1d4c
bhanusharma28 <bhanuhiet281100@gmail.com>
2025-03-19 16:56:13 +0530

    newlinechange

--- obj/Release/net8.0/PortfolioFrontend.AssemblyInfo.cs
+++ obj/Release/net8.0/PortfolioFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51de0caa852a4708173c788f478ac4ac95bd285")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3638fdbea973913beef179409e5cbe6f778cd04c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
